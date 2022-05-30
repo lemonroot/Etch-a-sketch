effd746fb238ec3df146eb556986d0daaa061159
lemonroot <79768526+lemonroot@users.noreply.github.com>
2022-05-30 18:26:20 -0400

    Hover event works
    On hover, grid cell turns gray - on mouseout, turns white.

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 
-const container = document.getElementById("container");
+const container = document.getElementById("gridcontainer");
 
 function makeRows(rows, cols){
     container.style.setProperty("--grid-rows", rows);
     container.style.setProperty("--grid-cols", cols);
     for(let c = 0; c < (rows * cols); c++){
         let cell = document.createElement("div");
-        cell.innerText = (c + 1);
         container.appendChild(cell).className = "grid-item";
     };
 };
+
+function hoverEvent(){
+    const items = document.querySelectorAll(".grid-item");
+    console.log(items);
+
+    items.forEach((item) => {
+        item.addEventListener("mouseover", function(e) {
+            item.setAttribute("style", "background-color: gray");
+        });
+        item.addEventListener("mouseout", function(e) {
+            item.setAttribute("style", "background-color: white");
+        });
+    });
+}
+
 makeRows(16,16);
+hoverEvent();
